5393e2a00d893eafa722a8698e03a2f530d09418
Patrizio Bekerle <patrizio@bekerle.com>
2022-01-05 16:53:26 +0100

    Start event implementation

---
+++ src/Event/PersonPhotoProviderPreEvent.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\GreenlightConnectorCampusonlineBundle\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+class PersonPhotoProviderPreEvent extends Event
+{
+    public const NAME = 'dbp.relay.greenlight_connector_campusonline.person_photo_provider.pre';
+
+    protected $userId;
+
+    public function __construct(string $userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+}
